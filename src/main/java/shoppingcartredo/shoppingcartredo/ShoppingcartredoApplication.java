7e3d1e5b4b742854cee9cb2546c0113cc10d6468
nchinling <nchinling@gmail.com>
2023-03-22 09:56:15 +0800

    setup

---
+++ src/main/java/shoppingcartredo/shoppingcartredo/ShoppingcartredoApplication.java
@@ -0,0 +1,13 @@
+package shoppingcartredo.shoppingcartredo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartredoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartredoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shoppingcartredo/shoppingcartredo/ShoppingcartredoApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppingcartredo.shoppingcartredo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartredoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
